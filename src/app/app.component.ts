4954236e0d20470149ea100555470cd8471107a1
Gopesh Sharma <gopesh.sharma@gmail.com>
2025-01-23 00:38:22 -0500

    fixed app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgclassComponent } from './components/ngclass/ngclass.component';
 import { NgstyleComponent } from './components/ngstyle/ngstyle.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NgstyleComponent],
+  imports: [NgstyleComponent, NgclassComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
